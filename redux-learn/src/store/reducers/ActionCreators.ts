8c57033ebd0700f5635bb95676ca19946b068a33
tishev5667 <tishev5667@bk.ru>
2023-08-13 23:15:52 +0300

    1
    1

---
+++ redux-learn/src/store/reducers/ActionCreators.ts
@@ -0,0 +1,21 @@
+import axios from 'axios';
+import { AppDispatch } from '../store';
+import { IUser } from '../../models/IUser';
+import { userSlice } from './UserSlice';
+import { createAsyncThunk } from '@reduxjs/toolkit';
+
+// export const fetchUsers = () => async (dispatch: AppDispatch) => {
+//   try {
+//     dispatch(userSlice.actions.usersFetching());
+//     const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+//     dispatch(userSlice.actions.usersFetchingSuccess(response.data));
+//   } catch (e) {
+//     dispatch(userSlice.actions.usersFetchingError('error 404'));
+//   }
+// };
+
+export const fetchUsers = createAsyncThunk('user/fetchAll', async (_, thunkApi) => {
+  const response = await axios.get<IUser[]>('https://jsonplaceholder.typicode.com/users');
+  return response.data;
+});
+//  19:25  Продвинутый Redux. Redux Toolkit, RTK query, TypeScript.
